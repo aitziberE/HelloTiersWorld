ff8c1b708156f8b4c877a81ea718e64a05eeaa1c
aitziberE <eskizabel.aitziber@gmail.com>
2024-09-23 11:33:59 +0200

    add DBUserDataAccessor

--- src/dataAccessTier/DBUserDataAccessor.java
+++ src/dataAccessTier/DBUserDataAccessor.java
@@ -5,6 +5,11 @@
  */
 package dataAccessTier;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import model.User;
 
 /**
@@ -12,10 +17,73 @@ import model.User;
  * @author Aitziber
  */
 public class DBUserDataAccessor implements DataAccessible{
-
+    private static final String GET_USER_DATA = "SELECT * FROM user";
+    
+    public Connection openConnection() {
+        // TODO Auto-generated method stub
+        Connection con = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/hellotiersdb?serverTimezone=Europe/Madrid&useSSL=false";
+            con = DriverManager.getConnection(url, "root", "");
+        } catch (SQLException e) {
+            // LOGGER: System.out.println("Error al intentar abrir la BD");
+        }
+        return con;
+    }
+    
+     public void closeConnection(PreparedStatement stmt, Connection con) throws SQLException {
+        // LOGGER System.out.println("Conexion cerrada");
+        if (stmt != null) {
+            stmt.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+    
+    
     @Override
     public User getUserData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        PreparedStatement stmt = null;
+        User user = null;
+
+        try {
+            con = openConnection();
+            stmt = con.prepareStatement(GET_USER_DATA);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                user = new User();
+                
+                // validar que el campo no sea null
+                
+                user.setFirstName(rs.getString("firstName"));
+                user.setLastName(rs.getString("lastName"));
+                user.setUserName(rs.getString("userName"));
+                user.setDni(rs.getString("dni"));
+                user.setEmail(rs.getString("email"));
+                java.sql.Date sqlDate = rs.getDate("birthDate");
+                
+                // refactorizar
+                if (sqlDate != null) {
+                    user.setBithDate(sqlDate.toLocalDate());
+                }
+                
+                user.setPassword(rs.getString("password"));
+            }
+
+        } catch (SQLException e) {
+            // LOGGER System.out.println("Error al obtener los datos del usuario: " + e.getMessage());
+        } finally {
+            try {
+                closeConnection(stmt, con);
+            } catch (SQLException e) {
+                // LOGGER System.out.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+        return user;
     }
     
 }
